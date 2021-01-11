c508b2299794f77400f54a9f1868ba5ee76702c5
Nicolas Hemenway <nhemenway@catalystsolves.com>
2021-01-11 12:53:04 -0500

    got backend security working properly

---
+++ src/main/java/io/training/catalyte/finalproject/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package io.training.catalyte.finalproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  private static final String[] AUTH_WHITELIST = {
+      // -- swagger ui
+      "/swagger-resources/**",
+      "/swagger-ui.html",
+      "/v2/api-docs",
+      "/webjars/**"
+  };
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedOrigins("http://localhost:3000");
+      }
+    };
+  }
+
+  @Override
+  public void configure(HttpSecurity http) throws Exception {
+    http
+        .authorizeRequests()
+        .antMatchers(AUTH_WHITELIST).permitAll()
+        .antMatchers(HttpMethod.GET).permitAll()
+        .antMatchers(HttpMethod.POST).permitAll()
+        .antMatchers(HttpMethod.PUT).permitAll()
+        .antMatchers(HttpMethod.DELETE).permitAll()
+        .and()
+        .sessionManagement().disable()
+        .csrf().disable();
+  }
+
+  @Override
+  public void configure(WebSecurity web) {
+    web.ignoring().antMatchers(HttpMethod.OPTIONS);
+    web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**",
+        "/configuration/security", "/swagger-ui.html", "/webjars/**");
+  }
+}
